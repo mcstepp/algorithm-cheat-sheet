4aff226a7404642e84f0fb619b41b76337e21b94
Meg Stepp <mcstepp@users.noreply.github.com>
2024-07-22 13:41:16 -0400

    linear search

---
+++ src/linear-search.ts
@@ -0,0 +1,11 @@
+// Explanation: TBD
+
+export default function linear_search(haystack: number[], needle: number): boolean {
+
+    for (let i = 0; i < haystack.length; ++i) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false;
+}
