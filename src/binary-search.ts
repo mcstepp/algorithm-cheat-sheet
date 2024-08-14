946a23cdf43a0a315b1fc0a82781bf6e10c7fdac
Meg Stepp <mcstepp@users.noreply.github.com>
2024-08-14 17:48:05 -0400

    binary search

---
+++ src/binary-search.ts
@@ -0,0 +1,16 @@
+// binary search
+
+// questions for datasets
+// 1. Is it ordered?
+//      If it's ordered, you have new options for searching
+
+// [            ]
+//  0          N
+// ---X0 = V?
+// you don't have to do this
+// you don't have to start at the first position, then the next, etc
+
+// instead, do half at a time
+// if its larger or smaller, do the left half or right half
+// continue halfing your dataset until you get to V
+// O(logN)
