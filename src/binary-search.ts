b758fa7428904faaf00242c15fa69abecc14a9cf
Meg Stepp <mcstepp@users.noreply.github.com>
2024-08-29 16:41:53 -0400

    binary search

--- src/binary-search.ts
+++ src/binary-search.ts
@@ -14,3 +14,37 @@
 // if its larger or smaller, do the left half or right half
 // continue halfing your dataset until you get to V
 // O(logN)
+
+// if you're scanning anything, O(NlogN)
+
+
+// search(arr, low, high, needle)
+//  midpoint = low + (high - low) / 2
+//  v = arr[midpoint]
+//  if v = needle, 
+//      return true
+// else if v > needle
+//      low = midpoint + 1
+// else
+//      high = midpoint
+
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        const midpoint = Math.floor(low + (high - low) / 2)
+        const value = haystack[midpoint]
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = midpoint;
+        } else {
+            low = midpoint + 1;
+        }
+
+    } while (low < high);
+
+    return false;
+}
